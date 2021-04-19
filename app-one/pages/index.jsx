7ace564f0bbe121dd28ebc69be3141e8cf6d48da
lukehedger <lh4@pm.me>
2021-04-19 14:29:45 +0100

    Setup Amplify monorepo apps

---
+++ app-one/pages/index.jsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello, from app one!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ app-two/pages/index.jsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello, from app two!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
